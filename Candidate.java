0ccfcda0fc034459767e007d2e2497d8aeb9e16e
Apollo <hackerapollo@gmail.com>
2025-01-28 18:56:39 +0000

    Created Candidate and Matchup classes and defined their attributes and methods.

---
+++ Candidate.java
@@ -0,0 +1,40 @@
+public class Candidate {
+    private String name;
+    private int wins;
+    private int losses;
+
+    public Candidate(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getWins()
+    {
+        return wins;
+    }
+    
+    public void setWins(int wins)
+    {
+        this.wins = wins;
+    }
+
+    public int getLosses()
+    {
+        return losses;
+    }
+
+    public void setLosses(int losses)
+    {
+        this.losses = losses;
+    }
+}
---
+++ Matchup.java
@@ -0,0 +1,51 @@
+public class Matchup {
+    private Candidate firstCanadate;
+    private Candidate secondCandidate;
+    private ArrayList<Integer> results = new ArrayList<Integer>();
+
+    public Matchup(Candidate firstCandidate, Candidate secondCandidate) 
+    {
+        this.firstCandidate = firstCandidate;
+        this.secondCandidate = secondCandidate;
+    }
+
+    public void addResult(int result) 
+    {
+        results.add(result);
+    }
+
+    public Candidate getWinner() 
+    {
+        int firstCandidateWins = 0;
+        int secondCandidateWins = 0;
+        for (int result : results) 
+        {
+            if (result == 1) {
+                firstCandidateWins++;
+            } else {
+                secondCandidateWins++;
+            }
+        }
+        if (firstCandidateWins >= secondCandidateWins) 
+        {
+            return firstCandidate;
+        } 
+        else 
+        {
+            return secondCandidate;
+        }
+    }
+
+    public double getWinPercentage()
+    {
+        int firstCandidateWins = 0;
+        for (int result : results) 
+        {
+            if (result == 1) 
+            {
+                firstCandidateWins++;
+            }
+        }
+        return (double) firstCandidateWins / (results.size());
+    }
+}
